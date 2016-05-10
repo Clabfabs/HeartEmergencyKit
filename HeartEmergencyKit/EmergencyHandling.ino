b9e3ca7996816df07d74c4157cf66aa534812f48
Sanaz Rahimi Moosavi <saramo@utu.fi>
2016-05-10 10:45:47 +0300

    send emergncy to bluetooth

--- HeartEmergencyKit/EmergencyHandling.ino
+++ HeartEmergencyKit/EmergencyHandling.ino
@@ -12,23 +12,23 @@ void checkEmergency() {
   if (wearing) {
     if (BPM < LOWER_THRESHOLD && emergency_low == false) {
       emergency_low = true;
-      Serial.println("EMERGENCY! Pulse is too low!");
-      Serial.print("BPM: ");
-      Serial.println(BPM);
+      bluetooth.println("EMERGENCY! Pulse is too low!");
+      bluetooth.print("BPM: ");
+      bluetooth.println(BPM);
     }
     else if (BPM > UPPER_THRESHOLD && emergency_high == false) {
       emergency_high = true;
-      Serial.println("EMERGENCY! Pulse is too high!");
-      Serial.print("BPM: ");
-      Serial.println(BPM);
+      bluetooth.println("EMERGENCY! Pulse is too high!");
+      bluetooth.print("BPM: ");
+      bluetooth.println(BPM);
     }
     else if (BPM >= LOWER_THRESHOLD && emergency_low == true) {
       emergency_low = false;
-      Serial.println("No low emergency anymore");
+      bluetooth.println("No low emergency anymore");
     }
     else if (BPM <= UPPER_THRESHOLD && emergency_high == true) {
       emergency_high = false;
-      Serial.println("No high emergency anymore");
+      bluetooth.println("No high emergency anymore");
     }
   }
 
